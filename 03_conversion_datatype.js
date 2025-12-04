91ac0172f1e19c084920c309647ada9374328b0f
Geeta Bangde <bangdegeeta@gmail.com>
2025-12-04 08:07:47 +0000

    convertion of datatype

---
+++ 03_conversion_datatype.js
@@ -0,0 +1,27 @@
+let age = 43;
+let score = "hitecg";
+let valueInNumber = Number(score);
+let isLogeddIn = 1
+
+// 1 => true;
+// 0 => false;
+// "hitesh" => true;
+
+// let booleanLogddenIn = Boolean(isLogeddIn);
+// console.log(booleanLogddenIn);
+// console.log(typeof booleanLogddenIn)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+//"33" => 33
+//"33sdfs" => NaN
+// true => 1 , false=> 2
+
+
+let someNumber = 33;
+let stringNumber =  String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber)
