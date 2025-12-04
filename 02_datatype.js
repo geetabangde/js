e0229cbf4f37996eb4f748115f535eff8bd406f0
Geeta Bangde <bangdegeeta@gmail.com>
2025-12-04 07:50:51 +0000

    datatype

---
+++ 02_datatype.js
@@ -0,0 +1,34 @@
+"use strict";
+//treat all js code as newer version
+
+// alert('hello')
+// we are using node js not browser
+console.log("hitesh");
+console.log(3 + 3);
+// code readability
+
+let name = 'geeta';
+let age = 98;
+let isLogin = false;
+let state = null;
+let city;
+console.log( typeof null); // object
+console.log(typeof undefined) // undefined
+
+// datatype Primative datatype
+// Number => 2 to power 53
+// bigint => bignumber
+// string => "";
+// boolean => true/false
+// null => object; //historical bug (standalone value)
+// undefined => value not assigne but varibale declared 
+// Symbol => compoent uniq idetifier using (react)
+
+//non-primative
+// Object 
+// array 
+
+
+
+
+
