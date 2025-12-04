7b811a29be2ccb599f91c7e905714663923c62db
Geeta Bangde <bangdegeeta@gmail.com>
2025-12-04 08:18:15 +0000

    operations

--- 03_conversion_datatype.js
+++ 03_conversion_datatype.js
@@ -21,8 +21,31 @@ let isLogeddIn = 1
 // true => 1 , false=> 2
 
 
-let someNumber = 33;
-let stringNumber =  String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber)
+// let someNumber = 33;
+// let stringNumber =  String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber)
 
+// ********************************* Operation***************************
+
+let value = 3
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2+3);
+
+// addition of stringn basic way
+let str1 = "hello";
+let str2 = 'sjs';
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2")
+console.log("1" - 2 + 2)
+console.log("1" % 2);
+console.log("1" / 2);
+console.log((2 + "2") * (3 % 4)); // do not used like that code 
