19cd95fd7c93481289e3458e15f633b1f995e57c
Geeta Bangde <bangdegeeta@gmail.com>
2025-12-04 07:32:20 +0000

    let, const, var

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 423424;
+let accountEMail = 's@gmail.com'
+var password = "12345";
+accountCity = 'jaipur'
+accountEMail = 'kdhks@gmail.com'
+accountCity = 'mumbai'
+let accountState;
+
+// accountId = 2
+console.log(accountEMail)
+console.table([accountCity, accountEMail, accountId, accountState])
